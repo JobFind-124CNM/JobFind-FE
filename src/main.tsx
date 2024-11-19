2f8a2bdc62b58eaf8f9c140cfa9e0d681507e180
lethanhtuan939 <thanhtuanle939@gmail.com>
2024-11-19 17:29:58 +0700

    Jobdetail page

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import JobDetail from "@/components/Job/JobDetail.tsx";
 
 const routers = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "posts/:id",
+    element: <JobDetail />,
   },
 ]);
 
